ede361c1ee5eb741fcf8137fb3168493bc96be5f
inyomansetiawan <insetiawan98@gmail.com>
2025-03-03 22:05:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def export_pdf(data, filename, logo_path):
             fontSize=12,  
             leading=18,
             alignment=TA_JUSTIFY,
-            bulletIndent=15 
+            firstLineIndent=-10
         )
         
         # Style untuk bullet list
