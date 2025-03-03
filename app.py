a748aed070aef6a2e9825157a349b7477305ae7a
inyomansetiawan <insetiawan98@gmail.com>
2025-03-03 22:29:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def export_pdf(data, filename, logo_path):
         # Style untuk bullet list
         bullet_style = ParagraphStyle(
             "bullet",
-            leftIndent=35,
+            leftIndent=25,
             bulletIndent=40,
             fontName="Lato-Regular", 
             fontSize=12,  
