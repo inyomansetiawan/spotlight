4453be57fbba3d8ef8a27447dec927a2aed1bed6
inyomansetiawan <insetiawan98@gmail.com>
2025-03-03 19:57:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def export_pdf(data, filename, logo_path):
         
                 # Jika bukan bullet atau numbered list, anggap sebagai teks biasa
                 in_numbered_list = False
-                elements_temp.append(Paragraph(line, answer_style1))
+                elements_temp.append(Paragraph(line, answer_style2))
                 elements_temp.append(Spacer(1, 6))
         
             # Tambahkan elemen-elemen ke dalam elements utama
